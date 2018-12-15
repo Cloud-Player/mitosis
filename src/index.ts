7705b664689a85a074aa36944ab425d798aea2e8
zarlex <alex.zarges@online.de>
2018-12-15 22:46:00 +0100

    Replace simple peer implementation with mitosis

--- src/index.ts
+++ src/index.ts
@@ -1,146 +1,133 @@
+import {filter} from 'rxjs/operators';
 import {Mitosis} from './mitosis';
+import {ConnectionState, IConnection} from './mitosis/connection/interface';
+import {RemotePeer} from './mitosis/mesh/remote-peer';
+import {ChurnType} from './mitosis/mesh/routing-table';
 
-const m = new Mitosis();
+class App {
+  private _mitosis: Mitosis;
 
-/*
-const peer = new Peer();
+  constructor() {
+    this._mitosis = new Mitosis();
+    this.listenOnInbox();
+    this.listenOnPeerChurn();
 
-document.querySelector('.clients ul')
-  .insertAdjacentHTML('afterbegin',
-    `<li>${peer.getId()} (me)</li>`
-  );
+    this.listenOnSubmitForm();
 
-document.querySelector('.send-message form').addEventListener('submit', (ev) => {
-  ev.preventDefault();
-  const form: HTMLFormElement = ev.currentTarget as HTMLFormElement;
-  const msg = (form.elements.namedItem('message') as HTMLInputElement).value;
-  const receiver = (form.elements.namedItem('receiver') as HTMLInputElement).value;
-
-  if (receiver === '*') {
-    peer.broadcastMessage(msg);
-  } else {
-    peer.sendMessageTo(parseInt(receiver, 10), msg);
+    document.querySelector('.clients ul')
+      .insertAdjacentHTML('afterbegin',
+        `<li>${this._mitosis.getMyAddress().getId()} (me)</li>`
+      );
   }
 
-  document.querySelector('.messages ul')
-    .insertAdjacentHTML('beforeend',
-      `<li class="outgoing"><span class="receiver">-> ${receiver}</span>: ${msg}</li>`
-    );
-});
+  private listenOnSubmitForm() {
+    document.querySelector('.send-message form').addEventListener('submit', (ev) => {
+      ev.preventDefault();
+      const form: HTMLFormElement = ev.currentTarget as HTMLFormElement;
+      const msg = (form.elements.namedItem('message') as HTMLInputElement).value;
+      const receiver = (form.elements.namedItem('receiver') as HTMLInputElement).value;
 
-peer.observe()
-  .pipe(
-    filter(ev => ev.type === PeerEventType.ADD_CONNECTION)
-  )
-  .subscribe((ev) => {
-      document.querySelector('.clients ul')
-        .insertAdjacentHTML('beforeend',
-          `<li id="${ev.body.peerId}">${ev.body.peerId}</li>`
-        );
+      if (receiver === '*') {
+        this.broadcastMessage(msg);
+      } else {
+        this.sendMessage(receiver, msg);
+      }
+    });
+  }
+
+  private sendMessage(peerId: string, message: string) {
+    if (!this._mitosis.getRoutingTable().getPeerById(peerId)) {
+      return;
     }
-  );
-
-peer.observe()
-  .pipe(
-    filter(ev => ev.type === PeerEventType.REMOVE_CONNECTION)
-  )
-  .subscribe((ev) => {
-      document.getElementById(ev.body.peerId).remove();
-    }
-  );
-
-peer.observe()
-  .pipe(
-    filter(ev => ev.type === PeerEventType.MESSAGE)
-  )
-  .subscribe((ev) => {
+    this._mitosis.sendMessageTo(peerId, message);
     document.querySelector('.messages ul')
       .insertAdjacentHTML('beforeend',
-        `<li class="incoming"><span class="originator"><- ${ev.body.from}</span>: ${ev.body.message}</li>`
+        `<li class="outgoing"><span class="receiver">-> ${peerId}</span>: ${message}</li>`
       );
-  });
-*/
+  }
 
-// var Peer = require('simple-peer')
-//
-// var peer1 = new Peer({ initiator: true })
-// var peer2 = new Peer()
-//
-// peer1.on('signal', function (data) {
-//   // when peer1 has signaling data, give it to peer2 somehow
-//   console.log(data);
-//   peer2.signal(data)
-// })
-//
-// peer2.on('signal', function (data) {
-//   // when peer2 has signaling data, give it to peer1 somehow
-//   peer1.signal(data)
-// })
-//
-// peer1.on('connect', function () {
-//   // wait for 'connect' event before using the data channel
-//   peer1.send('hey peer2, how is it going?')
-// })
-//
-// peer2.on('data', function (data) {
-//   // got a data channel message
-//   console.log('got a message from peer1: ' + data)
-// })
+  private broadcastMessage(message: string) {
+    this._mitosis.getRoutingTable().getPeers().forEach((peer) => {
+      this.sendMessage(peer.getId(), message);
+    });
+  }
 
-// const peer2 = new Peer(0);
-//
-// const obsPeer1 = Observable.create((observer: Observer<Connection>) => {
-//   peer1.observe()
-//     .pipe(
-//       filter(ev => ev.type === PeerEventType.CONNECTIONS_CREATED)
-//     )
-//     .subscribe(() => {
-//       observer.next(peer1.getConnections()[0]);
-//       observer.complete();
-//     });
-// });
-//
-// const obsPeer2 = Observable.create((observer: Observer<Connection>) => {
-//   peer2.observe()
-//     .pipe(
-//       filter(ev => ev.type === PeerEventType.CONNECTIONS_CREATED)
-//     )
-//     .subscribe(() => {
-//       observer.next(peer2.getConnections()[0]);
-//       observer.complete();
-//     });
-// });
-//
-// peer1.observe().pipe(
-//   filter(ev => ev.type === PeerEventType.CONNECTION_ESTABLISHED)
-// ).subscribe(() => {
-//   peer1.broadcastMessage('HELLO FROM PEER 1');
-// });
-//
-// peer1.observe().pipe(
-//   filter(ev => ev.type === PeerEventType.MESSAGE)
-// ).subscribe((ev) => {
-//   console.log('PEER 1 GOT MESSAGE ' + ev.body.content.toString());
-// });
-//
-// peer2.observe().pipe(
-//   filter(ev => ev.type === PeerEventType.CONNECTION_ESTABLISHED)
-// ).subscribe(() => {
-//   peer2.broadcastMessage('HELLO FROM PEER 2');
-// });
-//
-// peer2.observe().pipe(
-//   filter(ev => ev.type === PeerEventType.MESSAGE)
-// ).subscribe((ev) => {
-//   console.log('PEER 2 GOT MESSAGE ' + ev.body.content.toString());
-// });
-//
-// Promise.all([
-//   obsPeer1.toPromise()
-// ]).then((connections) => {
-//   const connectionPeer1: ConnectionOut = connections[0];
-//   peer2.requestConnection(connectionPeer1.getOffer())
-//     .then((connection: ConnectionIn) => {
-//       connectionPeer1.establish(connection.getAnswer());
-//     });
-// });
+  private listenOnInbox() {
+    this._mitosis.getInbox()
+      .subscribe((message) => {
+        document.querySelector('.messages ul')
+          .insertAdjacentHTML('beforeend',
+            `
+                    <li class="incoming">
+                      <span class="originator">
+                          <- ${message.getSender().getId()}
+                      </span>: ${message.getBody()}
+                      </li>
+                  `
+          );
+      });
+  }
+
+  private listenOnPeerChurn() {
+    this._mitosis.getRoutingTable().observePeerChurn()
+      .pipe(
+        filter(ev => ev.type === ChurnType.ADDED)
+      )
+      .subscribe(
+        ev => this.addPeer(ev.peer)
+      );
+
+    this._mitosis.getRoutingTable().observePeerChurn()
+      .pipe(
+        filter(ev => ev.type === ChurnType.REMOVED)
+      )
+      .subscribe(
+        ev => this.removePeer(ev.peer)
+      );
+  }
+
+  private addPeer(peer: RemotePeer) {
+    document.querySelector('.clients ul')
+      .insertAdjacentHTML('beforeend',
+        `
+                <li id="client_${peer.getId()}">
+                   ${peer.getId()} (<span class="connections">0</span>)
+                </li>
+              `
+      );
+    this.listenOnPeerConnectionChurn(peer);
+  }
+
+  private removePeer(peer: RemotePeer) {
+    const peerEl = document.getElementById(`client_${peer.getId()}`);
+    peerEl.remove();
+  }
+
+  private updateConnections(peer: RemotePeer) {
+    const peerEl = document.getElementById(`client_${peer.getId()}`);
+    const peerConnectionsEl = peerEl.querySelector('.connections');
+    const openConnections = peer.getConnectionTable().filterByStates(ConnectionState.OPEN);
+    peerConnectionsEl.innerHTML = peer.getConnectionTable().length.toString();
+    if (openConnections.length > 0) {
+      peerEl.classList.add('connected');
+    } else {
+      peerEl.classList.remove('connected');
+    }
+  }
+
+  private listenOnPeerConnectionChurn(peer: RemotePeer) {
+    peer.observeChurn()
+      .subscribe(ev => {
+        this.updateConnections(peer);
+        this.listenOnConnectionChanges(peer, ev.connection);
+      });
+  }
+
+  private listenOnConnectionChanges(peer: RemotePeer, connection: IConnection) {
+    connection.observeStateChange().subscribe(
+      ev => this.updateConnections(peer)
+    );
+  }
+}
+
+const app = new App();
