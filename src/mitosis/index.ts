ec0f4c22f1a6cbef15283a01706316c6e9918178
zarlex <alex.zarges@online.de>
2018-12-15 22:47:06 +0100

    Provide interfaces to observe incoming app-content messages and to send message to peer id

--- src/mitosis/index.ts
+++ src/mitosis/index.ts
@@ -1,3 +1,4 @@
+import {Subject} from 'rxjs';
 import {InternalClock} from './clock/internal';
 import {SecureEnclave} from './enclave/secure';
 import {MessageBroker} from './mesh/message-broker';
@@ -5,6 +6,8 @@ import {RemotePeer} from './mesh/remote-peer';
 import {RoleManager} from './mesh/role-manager';
 import {RoutingTable} from './mesh/routing-table';
 import {Address} from './message/address';
+import {AppContent} from './message/app-content';
+import {Message} from './message/message';
 import {RoleType} from './role/interface';
 
 export class Mitosis {
@@ -16,6 +19,7 @@ export class Mitosis {
   private _messageBroker: MessageBroker;
   private _myId: string;
   private _myAddress: Address;
+  private _inbox: Subject<Message>;
 
   public constructor(
     clock: IClock = new InternalClock(),
@@ -29,7 +33,16 @@ export class Mitosis {
     this._routingTable = new RoutingTable(this._myId);
     this._roleManager = new RoleManager(roles);
     this._messageBroker = new MessageBroker(this._routingTable, this._roleManager);
+    this._inbox = new Subject();
+    this.listenOnAppContentMessages();
     clock.onTick(this.onTick.bind(this));
+  }
+
+  private listenOnAppContentMessages() {
+    this._messageBroker.observeAppContentMessages()
+      .subscribe(
+        message => this._inbox.next(message)
+      );
   }
 
   private onTick(): void {
@@ -46,5 +59,22 @@ export class Mitosis {
 
   public getRoutingTable() {
     return this._routingTable;
+  }
+
+  public getInbox() {
+    return this._inbox;
+  }
+
+  public sendMessageTo(peerId: string, message: any) {
+    const existingPeer = this.getRoutingTable().getPeerById(peerId);
+    if (existingPeer) {
+      existingPeer.send(
+        new AppContent(
+          this.getMyAddress(),
+          new Address(existingPeer.getId()),
+          message
+        )
+      );
+    }
   }
 }
