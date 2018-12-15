8a7af624229ac0572815995fff24ea69787a32dd
zarlex <alex.zarges@online.de>
2018-12-15 22:44:43 +0100

    add sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const dev = process.env.NODE_ENV !== 'production';
 
@@ -8,11 +10,28 @@ const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: path.join(__dirname, '/src/index.html'),
   filename: 'index.html',
   inject: 'body',
+  minify: {
+    collapseWhitespace: true,
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    useShortDoctype: true
+  }
+});
+
+const MiniCssPluginConfig = new MiniCssExtractPlugin({
+  // Options similar to the same options in webpackOptions.output
+  // both options are optional
+  filename: "css/[name].[hash].css",
+  chunkFilename: "css/[id].css"
 });
 
 const DefinePluginConfig = new webpack.DefinePlugin({
   'process.env.NODE_ENV': JSON.stringify('production'),
 });
+
+const publicPath = '/';
 
 module.exports = {
   devServer: {
@@ -24,7 +43,10 @@ module.exports = {
     },
     historyApiFallback: true,
   },
-  entry: [path.join(__dirname, '/src/index.ts')],
+  entry: [
+    path.join(__dirname, '/src/index.ts'),
+    path.join(__dirname, '/src/index.scss')
+  ],
   devtool: 'inline-source-map',
   module: {
     rules: [
@@ -50,7 +72,11 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader',
+        use: [
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
@@ -59,7 +85,13 @@ module.exports = {
           limit: 10000,
         },
       },
-    ],
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.ts'],
@@ -67,6 +99,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
+    publicPath: publicPath
   },
   mode: dev ? 'development' : 'production',
   plugins: dev
@@ -74,5 +107,5 @@ module.exports = {
       HTMLWebpackPluginConfig,
       new webpack.HotModuleReplacementPlugin(),
     ]
-    : [HTMLWebpackPluginConfig, DefinePluginConfig],
+    : [HTMLWebpackPluginConfig, DefinePluginConfig, MiniCssPluginConfig, new OptimizeCSSAssetsPlugin({})],
 };
