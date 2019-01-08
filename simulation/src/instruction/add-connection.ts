b6b961f18ca06a7f84df55908658c5df1a4f30ec
Nicolas Drebenstedt <cutoffthetop@me.com>
2019-01-08 15:09:29 +0100

    Implement add and remove connection instructions

--- simulation/src/instruction/add-connection.ts
+++ simulation/src/instruction/add-connection.ts
@@ -1,3 +1,5 @@
+import {Address} from 'mitosis';
+import {MockConnection} from '../connection/mock';
 import {Simulation} from '../simulation';
 import {AbstractInstruction} from './instruction';
 import {IInstruction} from './interface';
@@ -5,5 +7,11 @@ import {IInstruction} from './interface';
 export class AddConnection extends AbstractInstruction implements IInstruction {
 
   public execute(simulation: Simulation): void {
+    const config = this.getConfiguration();
+    const from = Address.fromString(config.address);
+    const to = Address.fromString(config.target);
+    const connection = new MockConnection(from);
+    console.info(`👉 connect ${from.getId()} to ${to.getId()}`);
+    simulation.addConnection(from.getId(), to.getId(), connection);
   }
 }
