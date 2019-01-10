6fc93a68c92a8023e10470026b0c491f8548315f
zarlex <alex.zarges@online.de>
2019-01-10 13:22:20 +0100

    Check wether connection is open

--- simulation/src/simulation.ts
+++ simulation/src/simulation.ts
@@ -1,4 +1,13 @@
-import {Address, IClock, MasterClock, Message, Mitosis, Protocol, ProtocolConnectionMap} from 'mitosis';
+import {
+  Address,
+  ConnectionState,
+  IClock,
+  MasterClock,
+  Message,
+  Mitosis,
+  Protocol,
+  ProtocolConnectionMap
+} from 'mitosis';
 import {MockConnection} from './connection/mock';
 import {WebRTCMockConnection} from './connection/webrtc-mock';
 import {Edge} from './edge/edge';
@@ -103,10 +112,15 @@ export class Simulation {
     const edge = this._edges.get([to, from].join('-'));
     if (edge) {
       this._clock.setTimeout(() => {
-        (edge.getConnection() as MockConnection).onMessage(message);
+        const connection = (edge.getConnection() as MockConnection);
+        if (connection.getState() === ConnectionState.OPEN) {
+          connection.onMessage(message);
+        } else {
+          throw new Error(`mock connection failed to deliver message ${message.toString()} to edge ${to} because connection is not open!`);
+        }
       }, delay);
     } else {
-      throw new Error(`mock connection failed to deliver message ${message.toString()} to edge ${to}`);
+      throw new Error(`mock connection failed to deliver message ${message.toString()} to edge ${to} because connection does not exist!`);
     }
   }
 
