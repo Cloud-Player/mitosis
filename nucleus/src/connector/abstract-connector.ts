b1f5bfa0c7ca0a836d38fa6bb24833835e38520c
zarlex <alex.zarges@online.de>
2019-01-14 20:05:29 +0100

    Make nucleus narrensicher

--- nucleus/src/connector/abstract-connector.ts
+++ nucleus/src/connector/abstract-connector.ts
@@ -1,17 +1,12 @@
-import {
-  Address,
-  ConnectionState,
-  IConnection,
-  IConnectionOptions,
-  Message,
-  Protocol,
-  WebRTCConnection,
-  WebRTCStreamConnection
-} from 'mitosis';
+import {Address, ConnectionState, IConnection, IConnectionOptions, Message, WebRTCConnection} from 'mitosis';
+import {filter} from 'rxjs/operators';
 
 export abstract class AbstractConnector<TConnection extends WebRTCConnection> {
   private _inAreaElement: HTMLTextAreaElement;
   private _outAreaElement: HTMLTextAreaElement;
+  private _createOfferBtn: HTMLButtonElement;
+  private _createAnswerBtn: HTMLButtonElement;
+  private _establishBtn: HTMLButtonElement;
 
   protected _connections: Array<TConnection>;
 
@@ -53,11 +48,30 @@ export abstract class AbstractConnector<TConnection extends WebRTCConnection> {
     );
 
     this.beforeCreateAnswer(connection).then(() => {
-      connection.open().then(this.onOpen.bind(this));
+      connection.open().then(this.onOpen.bind(this, connection));
     });
 
     connection.observeStateChange().subscribe(
       (ev: ConnectionState) => this._outAreaElement.value = ev);
+  }
+
+  private pastedAnswer(): void {
+    this._createOfferBtn.disabled = true;
+    this._createAnswerBtn.disabled = true;
+    this._establishBtn.disabled = false;
+
+  }
+
+  private pastedOffer(): void {
+    this._createOfferBtn.disabled = true;
+    this._createAnswerBtn.disabled = false;
+    this._establishBtn.disabled = true;
+  }
+
+  private pastedUnknownInput() {
+    this._createOfferBtn.disabled = false;
+    this._createAnswerBtn.disabled = true;
+    this._establishBtn.disabled = true;
   }
 
   private establish(): void {
@@ -65,14 +79,57 @@ export abstract class AbstractConnector<TConnection extends WebRTCConnection> {
     this._connections[0].establish(answer);
   }
 
-  protected onOpen() {
+  protected onOpen(connection: IConnection) {
+    this._createOfferBtn.disabled = false;
+    this._createAnswerBtn.disabled = true;
+    this._establishBtn.disabled = true;
+    this._inAreaElement.value = '';
+    connection.observeStateChange()
+      .pipe(
+        filter(ev => ev === ConnectionState.CLOSED)
+      )
+      .subscribe(this.onClose.bind(this, connection));
+  }
 
+  protected onClose(connection: TConnection) {
+    const indexOfConnection = this._connections.indexOf(connection);
+    if (indexOfConnection !== -1) {
+      console.warn('Remove connection that was closed!');
+      this._connections.splice(indexOfConnection, 1);
+    }
+  }
+
+  protected bindInputArea(): void {
+    this._inAreaElement.addEventListener('input', () => {
+      const val = this._inAreaElement.value;
+      try {
+        const parsed: any = JSON.parse(val);
+        switch (parsed.type) {
+          case 'offer':
+            this.pastedOffer();
+            break;
+          case 'answer':
+            this.pastedAnswer();
+            break;
+          default:
+            this.pastedUnknownInput();
+            break;
+        }
+      } catch (err) {
+        this.pastedUnknownInput();
+      }
+    });
+
+    this._outAreaElement.addEventListener('click', () => {
+      this._outAreaElement.focus();
+      this._outAreaElement.select();
+    });
   }
 
   protected bindButtonListeners(): void {
-    document.querySelector(`${this.baseSelector} .create-offer-btn`).addEventListener('click', this.createOffer.bind(this));
-    document.querySelector(`${this.baseSelector} .create-answer-btn`).addEventListener('click', this.createAnswer.bind(this));
-    document.querySelector(`${this.baseSelector} .establish-btn`).addEventListener('click', this.establish.bind(this));
+    this._createOfferBtn.addEventListener('click', this.createOffer.bind(this));
+    this._createAnswerBtn.addEventListener('click', this.createAnswer.bind(this));
+    this._establishBtn.addEventListener('click', this.establish.bind(this));
   }
 
   protected beforeCreateOffer(connection: TConnection): Promise<void> {
@@ -86,6 +143,21 @@ export abstract class AbstractConnector<TConnection extends WebRTCConnection> {
   protected init(): void {
     this._inAreaElement = document.querySelector(`${this.baseSelector} .in-text-area`);
     this._outAreaElement = document.querySelector(`${this.baseSelector} .out-text-area`);
+    this._createOfferBtn = document.querySelector(`${this.baseSelector} .create-offer-btn`);
+    this._createAnswerBtn = document.querySelector(`${this.baseSelector} .create-answer-btn`);
+    this._establishBtn = document.querySelector(`${this.baseSelector} .establish-btn`);
+    this.pastedUnknownInput();
+    this._inAreaElement.value = '';
+    this._outAreaElement.value = `
+    1. Click on "Create offer" button\r
+    2. Copy offer that appears in this textarea\r
+    3. Open a new tab and paste the offer into the textarea above "Create offer button"\r
+    4. Click on "Create answer" button\r
+    5. Copy the answer that appears in this textarea\r
+    6. Go to the previous tab and paste the answer into the textarea above "Create offer button"\r
+    7. Click on "Establish" button
+    `;
     this.bindButtonListeners();
+    this.bindInputArea();
   }
 }
--- nucleus/src/connector/data-connector.ts
+++ nucleus/src/connector/data-connector.ts
@@ -13,6 +13,7 @@ import {AbstractConnector} from './abstract-connector';
 export class DataConnector extends AbstractConnector<WebRTCDataConnection> {
   private _inMessagesElement: HTMLTextAreaElement;
   private _outMessageElement: HTMLTextAreaElement;
+  private _outMessageFieldsetElement: HTMLFieldSetElement;
   private _sendElement: HTMLButtonElement;
 
   protected baseSelector = '.data-rtc';
@@ -39,10 +40,17 @@ export class DataConnector extends AbstractConnector<WebRTCDataConnection> {
     this._inMessagesElement.value = this._inMessagesElement.value += `\r${msg}`;
   }
 
-  protected onOpen() {
-    this._connections.forEach((connection: WebRTCDataConnection) => {
-      connection.observeMessageReceived().subscribe(this.onMessage.bind(this));
-    });
+  protected onOpen(connection: IConnection) {
+    super.onOpen(connection);
+    this._outMessageFieldsetElement.disabled = false;
+    connection.observeMessageReceived().subscribe(this.onMessage.bind(this));
+  }
+
+  protected onClose(connection: WebRTCDataConnection) {
+    super.onClose(connection);
+    if (this._connections.length === 0) {
+      this._outMessageFieldsetElement.disabled = true;
+    }
   }
 
   protected bindButtonListeners() {
@@ -54,6 +62,8 @@ export class DataConnector extends AbstractConnector<WebRTCDataConnection> {
     console.log(this.baseSelector);
     this._inMessagesElement = document.querySelector(`${this.baseSelector} .in-messages`);
     this._outMessageElement = document.querySelector(`${this.baseSelector} .out-message`);
+    this._outMessageFieldsetElement = document.querySelector(`${this.baseSelector} fieldset.out`);
+    this._outMessageFieldsetElement.disabled = true;
     this._sendElement = document.querySelector(`${this.baseSelector} .send-msg-btn`);
     super.init();
   }
