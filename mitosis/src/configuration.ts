4da483bf5de5d3ab6fe23afa4bb51006e2caf814
zarlex <alex.zarges@online.de>
2019-01-30 13:18:24 +0100

    add configs for connection meter

--- mitosis/src/configuration.ts
+++ mitosis/src/configuration.ts
@@ -2,5 +2,10 @@ export class Configuration {
   public static readonly DIRECT_CONNECTIONS_GOAL = 5;
   public static readonly DIRECT_CONNECTIONS_MAX = 10;
   public static readonly DEFAULT_SIGNAL_ADDRESS = 'mitosis/v1/p000/ws/localhost:8040/websocket';
-  public static ROUTER_REDIRECT_ALTERNATIVE_COUNT: 5;
+  public static readonly ROUTER_REDIRECT_ALTERNATIVE_COUNT = 5;
+  public static readonly TRANSMISSION_PING_INTERVAL = 4;
+  public static readonly SLIDING_WINDOW_SIZE = 12;
+  public static readonly CONNECTION_METER_OPEN_GRACE_PERIOD_TIME = 10;
+  public static readonly CONNECTION_METER_PROTECTION_TIME = 10;
+  public static readonly CONNECTION_METER_PUNISHMENT_TIME = 20;
 }
