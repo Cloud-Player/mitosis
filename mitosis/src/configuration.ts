648f250a64d54b5b0f87aed0040f848c9d2788fd
zarlex <alex.zarges@online.de>
2019-02-01 13:07:49 +0100

    add broadcast address

--- mitosis/src/configuration.ts
+++ mitosis/src/configuration.ts
@@ -1,4 +1,5 @@
 export class Configuration {
+  public static readonly BROADCAST_ADDRESS = '**';
   public static readonly DIRECT_CONNECTIONS_GOAL = 5;
   public static readonly DIRECT_CONNECTIONS_MAX = 10;
   public static readonly DEFAULT_SIGNAL_ADDRESS = 'mitosis/v1/p000/ws/localhost:8040/websocket';
