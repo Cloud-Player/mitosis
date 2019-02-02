e1dd6dd066e22098c56e650b2408357a57037a6e
zarlex <alex.zarges@online.de>
2019-02-03 00:11:01 +0100

    add log statement

--- mitosis/src/role/task/send-introduction.ts
+++ mitosis/src/role/task/send-introduction.ts
@@ -1,12 +1,14 @@
 import {Introduction} from '../../message/introduction';
 import {Mitosis} from '../../mitosis';
 import {RoleType} from '../interface';
+import {Logger} from '../../logger/logger';
 
 export function sendIntroduction(mitosis: Mitosis): void {
   const introduction = new Introduction(
     mitosis.getMyAddress(),
     mitosis.getSignalAddress()
   );
+  Logger.getLogger(mitosis.getMyAddress().getId()).debug('connecting to signal');
   mitosis.getPeerManager()
     .connectTo(mitosis.getSignalAddress())
     .then(
