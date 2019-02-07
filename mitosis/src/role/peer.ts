a04b3a595050e6810596b56fb742e98ac02525c3
zarlex <alex.zarges@online.de>
2019-02-07 11:33:02 +0100

    update task list

--- mitosis/src/role/peer.ts
+++ mitosis/src/role/peer.ts
@@ -2,23 +2,34 @@ import {Message} from '../message/message';
 import {Mitosis} from '../mitosis';
 import {RemotePeer} from '../peer/remote-peer';
 import {IRole} from './interface';
+import {acquireDirectConnections} from './task/acquire-direct-connections';
 import {closeDuplicateConnections} from './task/close-duplicate-connections';
 import {degradeToNewbie} from './task/degrade-to-newbie';
 import {ensureRouterConnection} from './task/ensure-router-connection';
 import {publishPeerUpdate} from './task/publish-peer-update';
+import {removeExpiredConnections} from './task/remove-expired-connections';
 import {removeSignal} from './task/remove-signal';
-import {satisfyConnectionGoal} from './task/satisfy-connection-goal';
+import {removeSuperfluousConnections} from './task/remove-superfluous-connections';
 import {sendAlternatives} from './task/send-alternatives';
 
 export class Peer implements IRole {
 
   public onTick(mitosis: Mitosis): void {
+    // Clean up connections
     closeDuplicateConnections(mitosis);
-    satisfyConnectionGoal(mitosis);
-    publishPeerUpdate(mitosis);
+    removeExpiredConnections(mitosis);
     removeSignal(mitosis);
+    removeSuperfluousConnections(mitosis);
+
+    // Organize roles
     degradeToNewbie(mitosis);
+
+    // Acquire connections
+    acquireDirectConnections(mitosis);
     ensureRouterConnection(mitosis);
+
+    // Publish
+    publishPeerUpdate(mitosis);
   }
 
   public onMessage(mitosis: Mitosis, message: Message): void {
