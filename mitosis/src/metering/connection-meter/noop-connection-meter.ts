a7256f51946b7cdd1f6000f0dbe8cda87df0ee39
zarlex <alex.zarges@online.de>
2019-02-07 11:29:40 +0100

    Overwrite isExpired and always return false

--- mitosis/src/metering/connection-meter/noop-connection-meter.ts
+++ mitosis/src/metering/connection-meter/noop-connection-meter.ts
@@ -1,11 +1,13 @@
-import {IClock} from '../../clock/interface';
-import {IConnection} from '../../connection/interface';
 import {ConnectionMeter} from './connection-meter';
 import {IConnectionMeter} from './interface';
 
 export class NoopConnectionMeter extends ConnectionMeter implements IConnectionMeter {
   public getQuality(): number {
     return 1;
+  }
+
+  public isLastSeenExpired() {
+    return false;
   }
 
   public start(): void {
