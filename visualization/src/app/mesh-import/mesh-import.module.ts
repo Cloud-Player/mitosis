06d0b316dbd7fe8e4c6bd183ae2b0fc57c5bd8bb
zarlex <alex.zarges@online.de>
2019-02-28 17:14:33 +0100

    add mesh visualizer for realtime socket subsriptions

--- visualization/src/app/mesh-import/mesh-import.module.ts
+++ visualization/src/app/mesh-import/mesh-import.module.ts
@@ -4,8 +4,10 @@ import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedModule} from '../shared/shared.module';
+import {MeshRealtimeVisualizerComponent} from './components/mesh-realtime-visualizer/mesh-realtime-visualizer';
 import {MeshVisualizerComponent} from './components/mesh-visualizer/mesh-visualizer';
 import {MeshImportRoutingModule} from './mesh-import.routes';
+import {SocketListenerService} from './services/socket-listener';
 
 @NgModule({
   imports: [
@@ -19,9 +21,12 @@ import {MeshImportRoutingModule} from './mesh-import.routes';
     MeshImportRoutingModule
   ],
   declarations: [
-    MeshVisualizerComponent
+    MeshVisualizerComponent,
+    MeshRealtimeVisualizerComponent
   ],
-  providers: []
+  providers: [
+    SocketListenerService
+  ]
 })
 export class MeshImportModule {
 }
