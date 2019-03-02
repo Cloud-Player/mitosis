6478455c80c0b560f1e848a34fdd436209c5ee4f
zarlex <alex.zarges@online.de>
2019-03-02 14:36:03 +0100

    add network configuration buttons and delete node button

--- visualization/src/app/simulation/components/sidebar/node-settings/node-settings.ts
+++ visualization/src/app/simulation/components/sidebar/node-settings/node-settings.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {Protocol} from 'mitosis';
-import {MockConnection, MockMediaStream, Node} from 'mitosis-simulation';
+import {Protocol, RoleType} from 'mitosis';
+import {MockConnection, Simulation} from 'mitosis-simulation';
 import {SimulationNodeModel} from '../../../src/simulation-node-model';
 
 @Component({
@@ -11,6 +11,9 @@ import {SimulationNodeModel} from '../../../src/simulation-node-model';
 export class NodeSettingsComponent implements OnInit, OnChanges {
   @Input()
   public selectedNode: SimulationNodeModel;
+
+  @Input()
+  public simulation: Simulation;
 
   public wssDelay: number;
   public webRTCDelay: number;
@@ -64,6 +67,39 @@ export class NodeSettingsComponent implements OnInit, OnChanges {
       return this.selectedNode.getMitosis().getRoleManager().getRoles();
     } else {
       return [];
+    }
+  }
+
+  public getRoleIcon(role) {
+    switch (role) {
+      case RoleType.ROUTER:
+        return 'fa fa-map-signs';
+      case RoleType.SIGNAL:
+        return 'fa fa-podcast';
+      case RoleType.NEWBIE:
+        return 'fa fa-user-o';
+      case RoleType.PEER:
+        return 'fa fa-user';
+    }
+  }
+
+  public deletePeer() {
+    this.simulation.removeNode(this.selectedNode.getMitosis());
+  }
+
+  public toggleNetwork() {
+    const existingNode = this.simulation.getNodeMap().get(this.selectedNode.getMitosis().getMyAddress().getId());
+    if (existingNode.hasNetwork()) {
+      existingNode.setHasNetwork(false);
+    } else {
+      existingNode.setHasNetwork(true);
+    }
+  }
+
+  public hasNetwork() {
+    const existingNode = this.simulation.getNodeMap().get(this.selectedNode.getMitosis().getMyAddress().getId());
+    if (existingNode) {
+      return existingNode.hasNetwork();
     }
   }
 
