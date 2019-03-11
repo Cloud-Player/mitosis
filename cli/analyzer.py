7c57f3c4a24c386947dd889270a56e2b919d8e1a
Nicolas Drebenstedt <cutoffthetop@me.com>
2019-03-11 18:55:54 +0100

    Handle missing router

--- cli/analyzer.py
+++ cli/analyzer.py
@@ -38,25 +38,26 @@ for mesh in mesh_series:
     if not mesh:
         continue
     router = None
-    ids = sorted(set(n['id'] for n in mesh))
+    ids = sorted(set(n['id'] for n in mesh if 'signal' not in n['id']))
     df = pd.DataFrame(0, index=ids, columns=ids)
     for node in mesh:
         if 'router' in node['roles']:
             router = node['id']
-        df.at[node['id'], node['id']] = 0
+        if node['id'] not in ids:
+            continue
         for proto in node['connections'].values():
             for peer in proto:
                 if peer in ids:
                     df.at[node['id'], peer] = 1
-    df.drop('signal', axis=0, inplace=True)
-    df.drop('signal', axis=1, inplace=True)
-    dist_matrix = csg.floyd_warshall(df, directed=False, unweighted=False)
+    dist_matrix = csg.dijkstra(df, directed=False, unweighted=False)
     dm = pd.DataFrame(dist_matrix, index=ids, columns=ids)
     dm = dm.replace(np.inf, np.NaN)
     dm = dm.replace(0.0, np.NaN)
     dm = dm.dropna(axis=0, how='all')
     dm = dm.dropna(axis=1, how='all')
     dm = dm.replace(np.NaN, 0.0)
+    if dm.get(router) is None:
+        continue
     connected_components, _ = csg.connected_components(dm.values)
     average_distance_to_router = dm.get(router).sum() / len(dm)
     analysis.append({
