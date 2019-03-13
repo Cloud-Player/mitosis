c6de53138c5dd0e27ce952bc8022415d05055c9c
Nicolas Drebenstedt <cutoffthetop@me.com>
2019-03-13 16:33:49 +0100

    Calculate largest component size and sort distribution

--- cli/analyzer.py
+++ cli/analyzer.py
@@ -88,13 +88,15 @@ for mesh_index, mesh in enumerate(mesh_series):
         connected_components, labels = csg.connected_components(dm.values)
         average_distance_to_router = dm.get(router).sum() / len(dm)
         components, _ = np.histogram(labels.T, labels.max() + 1)
-        component_distribution = ','.join(str(c) for c in components)
+        component_distribution = ','.join(str(c) for c in sorted(components))
+        largest_component = max(component_distribution)
         analysis.append({
             'average_distance_to_router': average_distance_to_router,
             'number_of_connected_nodes': len(dm),
             'number_of_total_nodes': len(mesh),
             'connected_components': connected_components,
-            'component_distribution': component_distribution
+            'component_distribution': component_distribution,
+            'largest_component': largest_component
         })
 
         if mesh_index == len(mesh_series) - 1:
